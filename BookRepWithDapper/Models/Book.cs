80ca7fc0081cb91be1d16375d885d71327db2b25
Mhastuurhah <70760998+Mhastuurhah@users.noreply.github.com>
2024-10-24 22:28:23 +0100

    Initial commit for my BookRep project

---
+++ BookRepWithDapper/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BookRepWithDapper.Models
+{
+    public class Book
+    {
+        public long ID { get; set; }
+        public string? BookName { get; set; }
+        public string? Author { get; set; }
+        public string? ISBN { get; set; }
+        public long YearPublished { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+        public DateTime? Deleted_At { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
---
+++ BookRepWithDapper/Models/BookInfoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BookRepWithDapper.Models
+{
+    public class BookInfoDTO
+    {
+        public required string? BookName { get; set; }
+        public required string? Author { get; set; }
+        public required string? ISBN { get; set; }
+        public required long YearPublished { get; set; }
+    }
+}

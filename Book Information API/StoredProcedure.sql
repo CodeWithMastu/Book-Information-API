5124810de03ef259610c8d1691f5c70c3d55b9ec
CodeWithMastu <masturah.moshood@gmail.com>
2024-10-25 14:33:10 +0100

    Create StoredProcedure.sql

---
+++ Book Information API/StoredProcedure.sql
@@ -0,0 +1,64 @@
+-- STORE PROCEDURES FOR BOOKREP DATABASE
+
+-- Add books to the database
+CREATE PROCEDURE AddBook
+    @BookName NVARCHAR(100),
+    @Author NVARCHAR(100),
+    @ISBN NVARCHAR(100),
+    @YearPublished BIGINT
+AS
+BEGIN
+    INSERT INTO BookInfo (BookName, Author, ISBN, YearPublished, Is_Deleted, Created_At, Updated_At, Deleted_At)
+    VALUES (@BookName, @Author, @ISBN, @YearPublished, 0, GETDATE(), NULL, NULL);
+END;
+
+-- Read/Get all the books from the database
+
+CREATE PROCEDURE GetAllBooks
+AS
+BEGIN
+    SELECT * FROM BookInfo WHERE Is_Deleted = 0;
+END;
+
+-- Read/ Get the books by ID
+
+CREATE PROCEDURE GetBookByID
+    @ID BIGINT
+AS
+BEGIN
+    SELECT * FROM BookInfo WHERE ID = @ID AND Is_Deleted = 0;
+END;
+
+-- Update the books in the database by ID
+CREATE PROCEDURE UpdateBook
+    @ID BIGINT,
+    @BookName NVARCHAR(100),
+    @Author NVARCHAR(100),
+    @ISBN NVARCHAR(100),
+    @YearPublished BIGINT,
+	@Updated_At DATETIME
+AS
+BEGIN
+    UPDATE BookInfo
+    SET BookName = @BookName,
+        Author = @Author,
+        ISBN = @ISBN,
+        YearPublished = @YearPublished,
+        Updated_At = @Updated_At
+    WHERE ID = @ID AND Is_Deleted = 0;
+END;
+
+-- Delete the book
+CREATE PROCEDURE DeleteBook
+	@ID BIGINT
+AS
+BEGIN
+    UPDATE BookInfo
+    SET Is_Deleted = 1,
+        Deleted_At = GETDATE()
+    WHERE ID = @ID;
+END;
+
+
+/*
+EXEC sp_helptext 'GetAllBooks';*/
